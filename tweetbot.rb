828eb34e9108ce850fa62140916f8895e21b9816
theaisforannie <annharter@gmail.com>
2014-12-27 14:05:56 -0800

    what am i even doing

--- tweetbot.rb
+++ tweetbot.rb
@@ -1,20 +1,10 @@
-
-
 require 'yaml'
 
-startup = YAML.load_file("startups.yml")
-thing = YAML.load_file("things.yml")
+startup = YAML.load_file("startups.yml").sample
+thing = YAML.load_file("things.yml").sample
 
 def tweet(startup, thing)
-
 	"Like #{startup}, but for #{thing}"
 end
 
-loop do
-	
-	puts tweet(startup.sample, thing.sample)
-
-	sleep 300
-
-end
-
+puts tweet(startup, thing)
